adddc5a049983e58e527e483a3b9d8fe7cc250ec
jinbiao <jinbiao@kuyumall.com>
2017-08-01 15:41:12 +0800

    添加TabLayout的使用

--- CoderToDeveloperByTCLer/app/src/main/java/com/example/jinboy/codertodeveloperbytcler/java_demo/appdemo/ui/AndroidUIActivity.java
+++ CoderToDeveloperByTCLer/app/src/main/java/com/example/jinboy/codertodeveloperbytcler/java_demo/appdemo/ui/AndroidUIActivity.java
@@ -1,28 +1,31 @@
 package com.example.jinboy.codertodeveloperbytcler.java_demo.appdemo.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.jinboy.codertodeveloperbytcler.java_demo.appdemo.R;
+import com.example.jinboy.codertodeveloperbytcler.java_demo.appdemo.ui.recyclerview.SwipeRefreshRecyclerViewActivity;
 
 public class AndroidUIActivity extends Activity implements View.OnClickListener{
 
-    private Button button1;
+    private Button ui_button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ui_activity);
-        button1 = (Button) findViewById(R.id.button1);
+        ui_button1 = (Button) findViewById(R.id.ui_button1);
+        ui_button1.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button1:
-
+            case R.id.ui_button1:
+                Intent intent = new Intent(this,SwipeRefreshRecyclerViewActivity.class);
+                startActivity(intent);
                 break;
 
         }
--- CoderToDeveloperByTCLer/app/src/main/java/com/example/jinboy/codertodeveloperbytcler/java_demo/appdemo/ui/fragment/FirstFragment.java
+++ CoderToDeveloperByTCLer/app/src/main/java/com/example/jinboy/codertodeveloperbytcler/java_demo/appdemo/ui/fragment/FirstFragment.java
@@ -3,6 +3,7 @@ package com.example.jinboy.codertodeveloperbytcler.java_demo.appdemo.ui.fragment
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +11,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.jinboy.codertodeveloperbytcler.java_demo.appdemo.R;
 import com.example.jinboy.codertodeveloperbytcler.java_demo.appdemo.entity.RecyclerEntity;
@@ -44,6 +46,12 @@ public class FirstFragment extends Fragment implements SwipeRefreshLayout.OnRefr
     private RecyclerEntity recyclerEntity = new RecyclerEntity();
     private EndlessRecyclerOnScrollListener mOnScrollListener;
 
+    private TabLayout tabLayout;
+    private List<String> list = new ArrayList();
+    /**标记选择位置**/
+    private int itemSelected = 0;
+    private String name = "全部";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -53,21 +61,19 @@ public class FirstFragment extends Fragment implements SwipeRefreshLayout.OnRefr
     }
 
     public void intView() {
-
         mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_layout);
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
         final LinearLayoutManager layoutManager = new LinearLayoutManager(this.getActivity());
         recyclerView.setLayoutManager(layoutManager);
-        getEntity();
+        getEntity(name);
         mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary,
                 R.color.blueLight,
                 R.color.btn_cm_bg_pressed,
                 R.color.withe);
         mSwipeRefreshLayout.setOnRefreshListener(this);
+        addTabLayout();
         addScrollListener();
     }
-
-
 
     private void addScrollListener () {
         if (mOnScrollListener != null) {
@@ -82,25 +88,86 @@ public class FirstFragment extends Fragment implements SwipeRefreshLayout.OnRefr
                         @Override
                         public void run() {
                             current = current + 1 ;
-                            getEntity();
+                            getEntity(name);
                         }
                     }, 500);
-
                 }
             }
         };
         recyclerView.addOnScrollListener(mOnScrollListener);
     }
 
-    public void getEntity() {
+    public void getData(){
+        list.add(name);
+        for(int i = 0; i <10; i++){
+            list.add("TCL产业" + i);
+        }
+    }
+
+    public void addTabLayout(){
+        tabLayout = (TabLayout)view.findViewById(R.id.tabLayout);
+        getData();
+        /**动态添加值**/
+        for (int i = 0; i < list.size(); i++) {
+            tabLayout.addTab(tabLayout.newTab());
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            if (tab != null) {
+                tab.setCustomView(R.layout.tab_layout);
+                View view = tab.getCustomView();
+                TextView textView = (TextView) view.findViewById(R.id.tv_txt);
+                textView.setText(list.get(i));
+            }
+        }
+        /**默认选择第一项itemSelected = 0 **/
+        TabLayout.Tab tab = tabLayout.getTabAt(itemSelected);
+        tab.select();
+
+        /**计算滑动的偏移量**/
+        final int width = (int) (getOffsetWidth(itemSelected) * getResources().getDisplayMetrics().density);
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tabLayout.scrollTo(width, 0);
+            }
+        });
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                itemSelected =tab.getPosition();
+                mSwipeRefreshLayout.setRefreshing(true);
+                isFirst = true;
+                name = list.get(itemSelected);
+                getEntity(name);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+
+    /**根据字符个数计算偏移量*/
+    private int getOffsetWidth(int index) {
+        String str = "";
+        for (int i = 0; i < index; i++) {
+            str += list.get(i);
+        }
+        return str.length() * 14 + index * 12;
+    }
+
+    public void getEntity(String name) {
         if(isFirst){
             current = 1;
             records.clear();
-            records.addAll( recyclerEntity.parseData(current,"全部"));
+            records.addAll( recyclerEntity.parseData(current,name));
             upDownRefreshAdapter= new UpDownRefreshAdapter(this,records);
             recyclerView.setAdapter(upDownRefreshAdapter);
         }else{
-            records.addAll(recyclerEntity.parseData(current,"全部"));
+            records.addAll(recyclerEntity.parseData(current,name));
             upDownRefreshAdapter.notifyDataSetChanged();
             }
         isLoading = (Integer.valueOf(pages) > Integer.valueOf(current));
@@ -112,7 +179,7 @@ public class FirstFragment extends Fragment implements SwipeRefreshLayout.OnRefr
     @Override
     public void onRefresh() {
         isFirst = true;
-        getEntity();
+        getEntity(name);
         mSwipeRefreshLayout.setRefreshing(false);
     }
 }
