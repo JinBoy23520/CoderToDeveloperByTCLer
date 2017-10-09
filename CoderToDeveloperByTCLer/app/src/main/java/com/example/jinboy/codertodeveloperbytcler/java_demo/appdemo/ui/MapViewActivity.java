0f3e335dfc593a3102fc429df1dbd9ccd89a13b3
jinbiao <jinbiao@kuyumall.com>
2017-10-09 16:27:16 +0800

    添加Android中国地图省份绘制点击demo

--- CoderToDeveloperByTCLer/app/src/main/java/com/example/jinboy/codertodeveloperbytcler/java_demo/appdemo/ui/MapViewActivity.java
+++ CoderToDeveloperByTCLer/app/src/main/java/com/example/jinboy/codertodeveloperbytcler/java_demo/appdemo/ui/MapViewActivity.java
@@ -1,10 +1,13 @@
 package com.example.jinboy.codertodeveloperbytcler.java_demo.appdemo.ui;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.jinboy.codertodeveloperbytcler.java_demo.appdemo.R;
 import com.example.jinboy.codertodeveloperbytcler.java_demo.appdemo.entity.MapViewEntity;
+import com.example.jinboy.codertodeveloperbytcler.java_demo.appdemo.ui.view.ChinaMapView;
 import com.example.jinboy.codertodeveloperbytcler.java_demo.appdemo.ui.view.MapView;
 
 import java.util.ArrayList;
@@ -13,6 +16,7 @@ import java.util.List;
 
 public class MapViewActivity extends AppCompatActivity {
     private List<MapViewEntity.Area> areaList;
+    ChinaMapView lView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,20 @@ public class MapViewActivity extends AppCompatActivity {
         MapView mapView = (MapView) findViewById(R.id.mapview);
         getData();
         mapView.init(areaList);
+        lView = (ChinaMapView)findViewById(R.id.vp);
+        lView.setOnProvinceSelectedListener(new ChinaMapView.OnProvinceSelectedListener() {
+            @Override
+            public void onprovinceSelected(ChinaMapView.Area pArea) {
+                Toast.makeText(MapViewActivity.this,"您选择了-->"+pArea.name(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        lView.setMapColor(Color.BLUE);
+        lView.setPaintColor(ChinaMapView.Area.HeBei, Color.rgb(0xfa,0x74,0x01),true);
+        lView.setPaintColor(ChinaMapView.Area.GuangDong, Color.rgb(0xd2,0x00,0x7f),true);
+        lView.setPaintColor(ChinaMapView.Area.BeiJing, Color.rgb(0x00,0x6f,0xbf),true);
+        lView.setPaintColor(ChinaMapView.Area.SiChuan, Color.rgb(0x00,0x9c,0x85),true);
+        lView.setPaintColor(ChinaMapView.Area.AnHui, Color.rgb(0x8f,0xc4,0x1e),true);
+//        lView.selectAProvince(ChinaMapView.Area.XinJiang);
     }
 
     public void getData(){
